51babcafcca3491beb94967231863e7e79b2025a
savioramon <savioramon12@gmail.com>
2021-09-14 22:30:13 -0300

    criando a primeira parte do chat (a tela de mensagens)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [mensagens, setMensagens] = useState([
+    {
+      texto: "Olá",
+    },
+
+    {
+      texto: "Tudo bem?",
+    }
+  ]);
+
   return (
     <div className="App">
-      ...
+      <div className="chat-tela">
+        <div className="chat">
+          
+          {
+            mensagens.map((mensagem)=>{
+              return (
+                <div className="caixa-mensagem">
+                  <p className="caixa-mensagem-texto">{mensagem.texto}</p>
+                </div>  
+              )
+            })
+          }
+
+        </div>
+      </div>
     </div>
   );
 }
