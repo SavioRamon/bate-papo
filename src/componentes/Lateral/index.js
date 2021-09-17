2f97f5298ec8bd2ecc02c7d88cea27472b17b318
savioramon <savioramon12@gmail.com>
2021-09-16 21:35:23 -0300

    chamando o redux no projeto e adicionando estilo no chat lateral selecionado

--- src/componentes/Lateral/index.js
+++ src/componentes/Lateral/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./style.css";
 
-function lateral(){
+import { useSelector } from "react-redux";
+
+function Lateral(){
+
+    const chatID = useSelector(state=>state.chats.chatID);
+
+    const estiloChatSelecionado = {
+        backgroundColor: "rgb(136, 207, 145)"
+    }
+
     return (
         <div className="lateral">
 
             <div className="chats">
                 <h2>Chats</h2>
-                <div className="geral">
+                <div 
+                    className="geral"
+                    style={chatID === 0? estiloChatSelecionado : {backGround: "red"}}
+                >
                     <p>GERAL</p>
                 </div>
             </div>
@@ -16,4 +28,4 @@ function lateral(){
     )
 }
 
-export default lateral;
+export default Lateral;
