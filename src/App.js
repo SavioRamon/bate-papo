00e10a77edc03c7bc1e5b4698f98d60118c0392d
savioramon <savioramon12@gmail.com>
2021-09-20 17:00:04 -0300

    criando o componente de chat

--- src/App.js
+++ src/App.js
@@ -2,56 +2,15 @@ import React, { useState } from "react";
 import './App.css';
 
 import Lateral from "./componentes/Lateral";
+import Chats from "./componentes/Chats";
 
 function App() {
-
-  const mensagens = [
-    {
-      texto: "oi"
-    },
-    {
-      texto: "tudo bem?"
-    }
-  ]
-
-  const [mensagemTexto, setMensagemTexto] = useState("");
-
   return (
-    <div className="App">
-
+    <div className="app">
       <Lateral />
-
-      <div className="chat-tela">
-        <div className="chat">
-          
-          {
-            mensagens.map((mensagem, key)=>{
-              return (
-                <div className="caixa-mensagem" key={key}>
-                  <p className="caixa-mensagem-texto">{mensagem.texto}</p>
-                </div>  
-              )
-            })
-          }
-
-        </div>
-
-        <div className="area-inputs">
-          <input
-            type="text"
-            className="input-mensagem" 
-            placeholder="Mensagem" 
-            value={mensagemTexto}
-            onChange={(value)=>{setMensagemTexto(value.target.value)}} 
-          />
-
-          <input type="button" className="input-botao-enviar" value="enviar" 
-            onClick={()=>mensagens.push({mensagemTexto})} />
-        </div>
-
-      </div>
+      
     </div>
-  );
+  )
 }
 
 export default App;
