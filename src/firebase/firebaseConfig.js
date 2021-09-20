212312b2ad30443d4c7d6d93a4efdf07037a6d23
savioramon <savioramon12@gmail.com>
2021-09-20 16:49:12 -0300

    configurando o firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANPFUvLz1bdp3WYe3Ofvp2cZi2e2wBmt8",
+    authDomain: "bate-papo-a748b.firebaseapp.com",
+    projectId: "bate-papo-a748b",
+    storageBucket: "bate-papo-a748b.appspot.com",
+    messagingSenderId: "189944284249",
+    appId: "1:189944284249:web:c228a10634bc2c835f1d48"
+  };
+  
+  export default firebaseConfig
