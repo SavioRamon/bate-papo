8c30af70fa43ad774ced8a217d05ec3caafc34ad
savioramon <savioramon12@gmail.com>
2021-09-21 16:09:52 -0300

    chamando a creator que edita o reducer

--- src/store/sagas/chats.js
+++ src/store/sagas/chats.js
@@ -2,7 +2,9 @@ import { call, put } from "redux-saga/effects";
 
 import { retornaMensagens } from "../../firebase";
 
+import { Creators as chatCreators } from "../ducks/chats";
+
 export function* getMensagens(dados) {
     const mensagens = yield call(retornaMensagens, dados.payload.chatID);
-    console.log(mensagens);
+    yield put(chatCreators.setMensagens(mensagens));
 }
