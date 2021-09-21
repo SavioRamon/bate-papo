69d493278d9d1edd27c85e661476d6ec5a00bd71
savioramon <savioramon12@gmail.com>
2021-09-21 15:33:50 -0300

    criando função que retorna as tarefas do firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -7,3 +7,8 @@ import firebaseConfig from "./firebaseConfig";
 const app = firebase.initializeApp(firebaseConfig);
 
 const db = app.firestore();
+
+export const retornaMensagens = async (chatID) => {
+    const mensagens = await db.collection("chats").doc(chatID).get().then(doc=>doc.data().mensagens);
+    return mensagens;
+}
