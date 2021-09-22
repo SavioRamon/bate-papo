915834121932a32b599a621d1f916bff7defebd6
savioramon <savioramon12@gmail.com>
2021-09-21 22:32:12 -0300

    chamando o redux no componente de chats e recebendo mensagens

--- src/componentes/Chats/index.js
+++ src/componentes/Chats/index.js
@@ -1,53 +1,63 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
+import { useSelector, useDispatch } from "react-redux";
+
+import { Creators as chatCreators } from "../../store/ducks/chats";
+
 function Chat() {
-    const mensagens = [
-        {
-          texto: "oi"
-        },
-        {
-          texto: "tudo bem?"
-        }
-      ]
-    
-      const [mensagemTexto, setMensagemTexto] = useState("");
-    
-      return (
+
+    const chatID = useSelector(state=>state.chats.chatID);
+    const mensagensFunction = useSelector(state=>state.chats.mensagensFunction);
+    const dispatch = useDispatch();
+
+    const [mensagens, setMensagens] = useState("");
+
+    const [mensagemTexto, setMensagemTexto] = useState("");
+
+    useEffect(()=>{
+        dispatch(chatCreators.getMensagens(chatID));
+    }, [])
+
+    useEffect(()=>{
+        mensagensFunction && mensagensFunction(setMensagens);
+    }, [mensagensFunction])
+
+    return (
         <div className="chat-componente">
     
-          <div className="chat-tela">
-            <div className="chat">
+        <div className="chat-tela">
+        <div className="chat">
               
-              {
-                mensagens.map((mensagem, key)=>{
-                  return (
+        { Array.isArray(mensagens) &&
+             mensagens.map((mensagem, key)=>{
+                return (
                     <div className="caixa-mensagem" key={key}>
-                      <p className="caixa-mensagem-texto">{mensagem.texto}</p>
+                        <p className="caixa-mensagem-texto">{mensagem.texto}</p>
                     </div>  
-                  )
-                })
-              }
+                )
+            })
+        }
     
-            </div>
+        </div>
     
-            <div className="area-inputs">
-              <input
+        <div className="area-inputs">
+            <input
                 type="text"
                 className="input-mensagem" 
                 placeholder="Mensagem" 
                 value={mensagemTexto}
                 onChange={(value)=>{setMensagemTexto(value.target.value)}} 
-              />
+            />
     
-              <input type="button" className="input-botao-enviar" value="enviar" 
+            <input type="button" className="input-botao-enviar" value="enviar" 
                 onClick={()=>mensagens.push({mensagemTexto})} />
             </div>
     
-          </div>
         </div>
-      );
+    </div>
+    );
 }
 
 export default Chat;
