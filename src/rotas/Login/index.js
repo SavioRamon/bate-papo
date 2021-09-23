18ee2702407bec342207b7ebab54643393c7c256
savioramon <savioramon12@gmail.com>
2021-09-23 16:40:33 -0300

    estrutura do formulário de login e estilo

--- src/rotas/Login/index.js
+++ src/rotas/Login/index.js
@@ -1,9 +1,52 @@
+import React, { useState } from "react";
+
 import "./style.css";
 
 function Login(){
+
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
     return (
         <div className="rota login">
-            Login
+            <form className="formulario-login">
+                <h1>Login</h1>
+                
+                <div className="divisao">
+                    <input 
+                        type="email"
+                        name="email"
+                        value={email} onChange={e=>setEmail(e.target.value)}
+                        placeholder="Email"
+                    />
+                </div>
+
+                <div className="divisao">
+                    <input 
+                    type="password" 
+                    name="senha" 
+                    value={senha} onChange={e=>setSenha(e.target.value)}
+                    placeholder="Senha"
+                />
+                </div>
+                
+                <div className="divisao-botoes">
+                    <input 
+                        type="button"
+                        name="Entrar"
+                        value="Entrar" onClick={()=>{}}
+                   /> 
+
+                   ou
+
+                   <input 
+                        type="button"
+                        name="Registrar"
+                        value="Registrar" onClick={()=>{}}
+                    />
+                </div>
+
+            </form>
         </div>
     )
 }
