5ce3d447b1b670bd3956ced038f433e8099c782c
savioramon <savioramon12@gmail.com>
2021-09-24 16:28:11 -0300

    criando sagas de login

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -4,11 +4,12 @@ import { Types as chatTypes } from "../ducks/chats";
 import { Types as usuarioTypes } from "../ducks/usuario";
 
 import { getMensagens } from "./chats";
-import { registrarUsuario } from "./usuario";
+import { registrarUsuario, loginUsuario } from "./usuario";
 
 export default function* rootSagas() {
     return yield all([
         takeLatest(usuarioTypes.REGISTRAR_USUARIO, registrarUsuario),
+        takeLatest(usuarioTypes.LOGIN_USUARIO, loginUsuario),
         takeLatest(chatTypes.GET_MENSAGENS, getMensagens),
     ])
 }
--- src/store/sagas/usuario.js
+++ src/store/sagas/usuario.js
@@ -1,8 +1,14 @@
 import { call, put } from "redux-saga/effects";
-import { novoUsuario } from "../../firebase";
+import { novoUsuario, fazerLogin } from "../../firebase";
 
 export function* registrarUsuario(dados){
     const { nome, email, senha } = dados.payload;
 
     yield call(novoUsuario, nome, email, senha);
+}
+
+export function* loginUsuario(dados) {
+    const { email, senha } = dados.payload;
+
+    yield call(fazerLogin, email, senha);
 }
