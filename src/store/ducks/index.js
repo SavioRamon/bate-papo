7182ad6dd551a43a967d49d3524f0afede65630c
savioramon <savioramon12@gmail.com>
2021-09-24 15:04:19 -0300

    configurando as types, creators e o reducer de usuário na store

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import chats from "./chats";
+import usuario from "./usuario";
 
 const rootReducer = combineReducers({
-    chats
+    chats,
+    usuario
 })
 
 export default rootReducer;
---
+++ src/store/ducks/usuario.js
@@ -0,0 +1,38 @@
+const Types = {
+    REGISTRAR_USUARIO: "REGISTRAR_USUARIO_ASYNC",
+    SET_USUARIO: "SET_USUARIO"
+}
+
+const Creators = {
+    registrarUsuario: (email, senha)=>({
+        type: Types.REGISTRAR_USUARIO,
+        payload: {
+            email, 
+            senha
+        }
+    }),
+
+    setUsuario: (dadosUsuario)=>({
+        type: Types.SET_USUARIO,
+        payload: {
+            dadosUsuario
+        }
+    })
+}
+
+const STATE_INICIAL = {
+    usuario: null
+}
+
+export default function usuario(state=STATE_INICIAL, { type, payload }) {
+    switch(type) {
+        case Types.SET_USUARIO:
+            return {
+                ...state,
+                usuario: payload.dadosUsuario
+            }
+        
+        default:
+            return state;
+    }
+}
