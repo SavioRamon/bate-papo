718437bfbcd7784ca926de224a4a71ffd5bd758c
savioramon <savioramon12@gmail.com>
2021-09-24 21:35:35 -0300

    criando sagas que adiciona no reducer os dados do usuário

--- src/store/sagas/usuario.js
+++ src/store/sagas/usuario.js
@@ -1,14 +1,27 @@
 import { call, put } from "redux-saga/effects";
-import { novoUsuario, fazerLogin } from "../../firebase";
+import { retornaDadosUsuario, novoUsuario, fazerLogin } from "../../firebase";
+
+import { Creators as usuarioCreators } from "../ducks/usuario";
+
+function* setUsuarioNoReducer(usuarioID) {
+    const dadosUsuario = yield call(retornaDadosUsuario, usuarioID);
+
+    yield put(usuarioCreators.setUsuario(dadosUsuario));
+}
 
 export function* registrarUsuario(dados){
     const { nome, email, senha } = dados.payload;
 
-    yield call(novoUsuario, nome, email, senha);
+    const usuarioID = yield call(novoUsuario, nome, email, senha);
+
+    yield call(setUsuarioNoReducer, usuarioID);
+
 }
 
 export function* loginUsuario(dados) {
     const { email, senha } = dados.payload;
 
-    yield call(fazerLogin, email, senha);
+    const usuarioID = yield call(fazerLogin, email, senha);
+
+    yield call(setUsuarioNoReducer, usuarioID);
 }
