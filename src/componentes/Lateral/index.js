a85410919f2a599391d9fbcbe3e5f6d5b1a2cfad
savioramon <savioramon12@gmail.com>
2021-09-28 17:22:27 -0300

    começando a criação do cabeçalho

--- src/componentes/Lateral/index.js
+++ src/componentes/Lateral/index.js
@@ -1,22 +1,46 @@
 import React from "react";
 import "./style.css";
 
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { Link } from "react-router-dom";
 
+import { useSelector } from "react-redux";
+
 function Lateral(){
+
+    const dadosUsuario = useSelector(state=>state.usuario.dadosUsuario);
 
     return (
         <div className="lateral">
 
             <div className="lateral-cabecalho">
-                
-                <Link to="/login" className="link selecao">
-                    Login
-                </Link>
+                {dadosUsuario?
 
-                <Link to="/registrar" className="link selecao">
-                    Registrar
-                </Link>
+                    <div className="cabecalho-conteudo">
+                        <div className="perfil-usuario">
+
+                        </div>
+                        <div className="icone-opcoes">
+                            <FontAwesomeIcon icon={ faEllipsisV } />
+                        </div>
+                    </div>
+
+                    :
+
+                    <React.Fragment>
+                        <Link to="/login" className="link selecao">
+                            Login
+                        </Link>
+
+                        <Link to="/registrar" className="link selecao">
+                            Registrar
+                        </Link>
+                    </React.Fragment>
+
+                }
+                
 
             </div>
 
