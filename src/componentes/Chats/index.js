a5104ea127ba59d4b16d30c3911c88b0276cfd56
savioramon <savioramon12@gmail.com>
2021-10-06 18:09:19 -0300

    exibindo informações do usuario ao clicar em sua mensagem

--- src/componentes/Chats/index.js
+++ src/componentes/Chats/index.js
@@ -2,24 +2,27 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { useSelector, useDispatch } from "react-redux";
 
 import { Creators as chatCreators } from "../../store/ducks/chats";
 
 function Chat() {
     const dadosUsuario = useSelector(state=>state.usuario.dadosUsuario);
-
     const chats = useSelector(state=>state.chats);
     const dispatch = useDispatch();
 
     const [mensagens, setMensagens] = useState("");
-
     const [mensagemEnviar, setMensagemEnviar] = useState({
         imagem: "",
         remetente: "",
         idUsuario: "",
         texto: ""
     });
+
+    const [mostraInfoUsuario, setMostraInfoUsuario] = useState(false);
 
     useEffect(()=>{
         dispatch(chatCreators.getMensagens(chats.chatID));
@@ -41,73 +44,120 @@ function Chat() {
     }, [dadosUsuario])
 
     return (
-        <div className="chat-componente">
-    
-        <div className="chat-tela">
-        <div className="chat">
-              
-        { Array.isArray(mensagens) &&
-             mensagens.map((mensagem, key)=>{
-                let mensagemRemetente = "outro-usuario";
-                if(dadosUsuario) {
-                    mensagemRemetente = mensagem.idUsuario === dadosUsuario.id?
-                    "usuario-principal"
-                    :
-                    "outro-usuario";
-                }
-                
-                return (
-                    <div className={`area-mensagem`} key={key}>
-
-                        <div className={`mensagem ${mensagemRemetente}`}>  
-                            <img src={mensagem.imagem} />
-
-                            <div className="conteudo">
-                                
-                                {mensagemRemetente === "outro-usuario" &&
-                                    <p className="nome-remetente">{mensagem.remetente}</p>
-                                }
-                                
-                                <p className="mensagem-texto">{mensagem.texto}</p>
-                            </div>
-                        </div>
-                    </div>  
-                )
-            })
+    <div className="chat-componente">
+        
+        {mostraInfoUsuario && 
+            <InfoUsuario dados={mostraInfoUsuario} mostraInfo={setMostraInfoUsuario} />
         }
-    
-        </div>
-    
-        <div className="area-inputs">
-            <input
-                type="text"
-                className="input-mensagem" 
-                placeholder="Mensagem" 
-                value={mensagemEnviar.texto}
-                onChange={(e)=>{
-                    setMensagemEnviar({
-                        ...mensagemEnviar,
-                        texto: e.target.value
-                    })
-                }} 
-            />
-    
-            <input type="button" className="input-botao-enviar" value="enviar" 
-                onClick={()=>{
+
+        <div className="chat-tela">
+
+            <div className="chat">  
+            { Array.isArray(mensagens) &&
+                mensagens.map((mensagem, key)=>{
+                    
+
+                    let mensagemRemetente = "outro-usuario";
                     if(dadosUsuario) {
-                        dispatch(chatCreators.enviaMensagem(chats.chatID, mensagemEnviar))
-                        setMensagemEnviar({
-                            ...mensagemEnviar,
-                            texto: ""
-                        });
+                        mensagemRemetente = mensagem.idUsuario === dadosUsuario.id?
+                        "usuario-principal"
+                        :
+                        "outro-usuario";
                     }
                     
-                }} />
+                    return (
+                        <div className={`area-mensagem`} key={key}>
+
+                            <div className={`mensagem ${mensagemRemetente}`}>  
+                                <img 
+                                    className="imagem-perfil" 
+                                    alt="imagem-perfil" 
+                                    src={mensagem.imagem} 
+                                    onClick={()=>{
+                                        mensagem.idUsuario !== dadosUsuario.id && setMostraInfoUsuario(mensagem);
+                                    }}
+                                />
+
+                                <div className="conteudo">
+
+                                    {mensagemRemetente === "outro-usuario" &&
+                                        <p className="nome-remetente">{mensagem.remetente}</p>
+                                    }
+                                    
+                                    <p className="mensagem-texto">{mensagem.texto}</p>
+                                </div>
+                            </div>
+                        </div>  
+                    )
+                })
+            }
+    
             </div>
     
+            <div className="area-inputs">
+                <input
+                    type="text"
+                    className="input-mensagem" 
+                    placeholder="Mensagem" 
+                    value={mensagemEnviar.texto}
+                    onChange={(e)=>{
+                        setMensagemEnviar({
+                            ...mensagemEnviar,
+                            texto: e.target.value
+                        })
+                    }} 
+                />
+        
+                <input type="button" className="input-botao-enviar" value="enviar" 
+                    onClick={()=>{
+                        if(dadosUsuario) {
+                            dispatch(chatCreators.enviaMensagem(chats.chatID, mensagemEnviar))
+                            setMensagemEnviar({
+                                ...mensagemEnviar,
+                                texto: ""
+                            });
+                        }
+                        
+                    }} />
+            </div>
+        
         </div>
     </div>
     );
+}
+
+function InfoUsuario({ dados, mostraInfo }) {
+
+    const [infoOpcoes, setInfoOpcoes] = useState(false);
+
+    return (
+        <div className="tela-escurecida">
+            <div className="info-usuario">
+
+                <div className="info-usuario-area-superior">
+                    <input type="button" value={"<"} onClick={()=>mostraInfo(false)} />
+
+                    <p className="info-nome">{dados.remetente}</p>
+
+                    <div className="info-opcoes" onClick={()=>{
+                        setInfoOpcoes(!infoOpcoes);
+                    }}>
+                        <FontAwesomeIcon icon={ faEllipsisV } />
+                    </div>
+                </div>
+
+                {infoOpcoes &&
+                    <div className="opcoes">
+                        <p onClick={()=>{
+                            setInfoOpcoes(false);
+                        }}>Conversar com {dados.remetente}</p>
+                    </div>
+                }
+
+                <img src={dados.imagem} alt="imagem-perfil"/>
+            </div>
+        </div>
+    )
 }
 
 export default Chat;
