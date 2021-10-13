c9e5e1dd4f0533c505dc6134e42b133a18cdcb3f
savioramon <savioramon12@gmail.com>
2021-10-13 18:16:12 -0300

    adicionando ícone de voltar

--- src/componentes/PerfilConfig/index.js
+++ src/componentes/PerfilConfig/index.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import "./style.css";
 
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { Creators as usuarioCreators } from "../../store/ducks/usuario";
 import { useSelector, useDispatch } from "react-redux";
+
 
 function PerfilConfig() {
 
@@ -12,53 +16,67 @@ function PerfilConfig() {
     const dadosUsuarioEdicao = {
         ...dadosUsuario
     }
-
     const [alteraNome, setAlteraNome] = useState(dadosUsuario.nome);
 
     const [abrirConfig, setAbrirConfig] = useState(false);
+
     return (
         <React.Fragment>
             <img 
                 className="perfil-botao" 
-                onClick={()=>setAbrirConfig(!abrirConfig)}
+                onClick={()=>setAbrirConfig(true)}
                 src={dadosUsuario.imagem}
             />
 
             {abrirConfig &&
-                <div className="tela-configuracoes">
-                    <label 
-                        htmlFor='alterar-imagem-perfil' 
-                        className="imagem-perfil"
-                        style={{backgroundImage: `url(${dadosUsuario.imagem})`}}></label>
-                    <input type='file' id='alterar-imagem-perfil' onChange={e=>{
-                        dispatch(usuarioCreators.editarImagem(dadosUsuario.id, e.target.files[0]));
-                    }}/>
+                <React.Fragment>
+                    <div className="icone-voltar" onClick={()=>setAbrirConfig(false)}>
+                        <FontAwesomeIcon icon={ faArrowLeft } />
+                    </div>
 
-                    <div className="alterar-dados-conta">
-                        <div className="area-alterar-nome">
-                            <label htmlFor="alterar-nome">Nome: </label>
-                            <input 
-                                type="text"
-                                id="alterar-nome"
-                                value={alteraNome} onChange={(e)=>setAlteraNome(e.target.value)} 
-                                className="alterar-nome"
-                                maxLength="10"
-                            />
+                    <div className="tela-configuracoes">
+                        <div className="area-superior">
+                            <label 
+                                htmlFor='alterar-imagem-perfil' 
+                                className="imagem-perfil"
+                                style={{backgroundImage: `url(${dadosUsuario.imagem})`}}></label>
+                            <input type='file' id='alterar-imagem-perfil' onChange={e=>{
+                                dispatch(usuarioCreators.editarImagem(dadosUsuario.id, e.target.files[0]));
+                            }}/>
                         </div>
                         
-                        <input 
-                            type="button" 
-                            value="Salvar" 
-                            onClick={()=>{
-                                dispatch(usuarioCreators.editarUsuario({
-                                    ...dadosUsuarioEdicao,
-                                    nome: alteraNome
-                                }))
-                            }}
-                            className="salvar-alteracoes"
-                        />
+
+                        <div className="alterar-dados-conta">
+                            <div className="area-alterar-nome">
+                                <label htmlFor="alterar-nome">Nome: </label>
+                                <input 
+                                    type="text"
+                                    id="alterar-nome"
+                                    value={alteraNome} onChange={(e)=>setAlteraNome(e.target.value)} 
+                                    className="alterar-nome"
+                                    maxLength="10"
+                                />
+                            </div>
+                            
+                            <input 
+                                type="button" 
+                                value="Salvar" 
+                                onClick={()=>{
+
+                                    dispatch(usuarioCreators.editarUsuario({
+                                        ...dadosUsuarioEdicao,
+                                        nome: alteraNome
+                                    }));
+
+                                    setAbrirConfig(false);
+                                }}
+                                className="salvar-alteracoes"
+                            />
+                        </div>
                     </div>
-                </div>
+                </React.Fragment>
+                
+                
 
             }
         </React.Fragment>
