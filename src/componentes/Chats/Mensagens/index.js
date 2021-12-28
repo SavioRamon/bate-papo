0706de99fc70769069e39229d7b0357a745ea5c6
savioramon <savioramon12@gmail.com>
2021-12-28 17:48:37 -0300

    removendo importação desnecessária

--- src/componentes/Chats/Mensagens/index.js
+++ src/componentes/Chats/Mensagens/index.js
@@ -1,4 +1,4 @@
-import React, { useState, memo } from "react";
+import React, { memo } from "react";
 import "./style.css";
 
 import { Creators as componentesCreators } from "../../../store/ducks/componentes";
